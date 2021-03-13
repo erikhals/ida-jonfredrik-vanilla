3b2e936b83ef4c8facf7c980d823ae36920562f8
Erik Hals <erikhals@gmail.com>
2021-03-13 15:27:38 +0100

    Implement iframe

--- main.js
+++ main.js
@@ -1,43 +1,10 @@
-window.addEventListener("load", function () {
-  function sendData() {
-    const XHR = new XMLHttpRequest();
-
-    // Bind the FormData object and the form element
-    const FD = new FormData(form);
-
-    // Define what happens on successful data submission
-    XHR.addEventListener("load", function (event) {
-      alert(event.target.responseText);
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener("error", function (event) {
-      console.log("There is an error, but the data is probably submitted.");
-    });
-
-    // Set up our request
-    XHR.open(
-      "POST",
-      "https://docs.google.com/forms/u/0/d/e/1FAIpQLSeRh9cfECN8L02X67cSAGRz6HZN4oD-aKjGlXux_b-AOZ-j1g/formResponse",
-      true
-    );
-    // XHR.setRequestHeader("Accept", "application/xml, text/xml, */*; q=0.01");
-    // XHR.setRequestHeader(
-    //   "Content-type",
-    //   "application/x-www-form-urlencoded; charset=UTF-8"
-    // );
-    // The data sent is what the user provided in the form
-    XHR.send(FD);
-  }
-
-  // Access the form element...
-  const form = document.getElementById("svarForm");
-
-  // ...and take over its submit event.
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    sendData();
+// set the target on the form to point to a hidden iframe
+// some browsers need the target set via JavaScript, no idea why...
+document.getElementById("svarForm").target = "my-response-iframe";
+// detect when the iframe reloads
+var iframe = document.getElementById("my-response-iframe");
+if (iframe) {
+  iframe.onload = function () {
     //Show alert
     document.querySelector(".alert").style.display = "block";
     document.getElementById("svarForm").reset();
@@ -46,5 +13,6 @@ window.addEventListener("load", function () {
     setTimeout(function () {
       document.querySelector(".alert").style.display = "none";
     }, 4000);
-  });
-});
+    // now you can do stuff, such as displaying a message or redirecting to a new page.
+  };
+}
