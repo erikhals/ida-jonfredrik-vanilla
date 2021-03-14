c4234451eddde70226610e74a24d39d6e3a92527
Erik Hals <erikhals@gmail.com>
2021-03-14 01:43:41 +0100

    Set iframe to about blank

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // set the target on the form to point to a hidden iframe
 // some browsers need the target set via JavaScript, no idea why...
+
 document.getElementById("svarForm").target = "my-response-iframe";
+
 // detect when the iframe reloads
 var iframe = document.getElementById("my-response-iframe");
+iframe.src = "about:blank";
 if (iframe) {
   iframe.onload = function () {
     //Show alert
@@ -13,7 +16,5 @@ if (iframe) {
     setTimeout(function () {
       document.querySelector(".alert").style.display = "none";
     }, 4000);
-
-    iframe.parentNode.removeChild(iframe);
   };
 }
